8c55ae31d3f8edf17dfe2e545b03bd48c7f3ce74
SavvyProp <ludwigtaycheeying@gmail.com>
2024-09-25 19:58:32 -0400

    urdf and sdl

--- hrc_handler/setup.py
+++ hrc_handler/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'hrc_handler'
 
@@ -10,6 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/meshes', glob('meshes/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
+        ('share/' + package_name + '/config', glob('config/*')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
